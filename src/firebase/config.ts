85c3913b3c40036ee010cdb1333f7dc58fbc4a70
Luca Grigore <lucagrigore28@gmail.com>
2026-01-03 19:43:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-524597312-3104b",
+  "appId": "1:451317985684:web:5f70b71ee8dab7346b5f81",
+  "apiKey": "AIzaSyCcD3JASDRZYeRnGSEakgF8-yKRmSpyYJw",
+  "authDomain": "studio-524597312-3104b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "451317985684"
+};
