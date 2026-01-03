5b823888d377c86552f6ef9401fb5822933d8191
Luca Grigore <lucagrigore28@gmail.com>
2026-01-03 15:21:04 +0000

    versiunea anterioara era mai buna

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -44,7 +44,7 @@ export default function Logo({ className }: { className?: string }) {
           H 69
           C 72.3137 75 75 72.3137 75 69
           V 47
-          H 60 L 48 62 L 40 47 H 25 Z
+          H 60 L 48 62 L 40 54 H 25 Z
         " fill="url(#instaGradient)" stroke="none"/>
         
       </g>
