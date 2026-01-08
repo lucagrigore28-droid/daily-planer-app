aa7c9a09f4c3586c662c620363f8598d61c02374
Luca Grigore <lucagrigore28@gmail.com>
2026-01-08 13:07:25 +0000

    Try fixing this error: `Console Error: [2026-01-08T13:07:01.325Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,40 +3,24 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
+  let app: FirebaseApp;
   if (!getApps().length) {
-    let firebaseApp;
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-    return getSdks(firebaseApp);
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApp();
   }
-  return getSdks(getApp());
+  return getSdks(app);
 }
 
 export function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
   const auth = getAuth(firebaseApp);
 
-  if (process.env.NODE_ENV !== 'production') {
-    try {
-      connectAuthEmulator(auth, 'http://127.0.0.1:9099', {
-        disableWarnings: true,
-      });
-      connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-    } catch (e) {
-        // console.warn("Could not connect to emulators", e)
-    }
-  }
   return {
     firebaseApp,
     auth,
