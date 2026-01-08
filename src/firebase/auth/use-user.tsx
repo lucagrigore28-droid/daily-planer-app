6979e82126b420b5520a698f21aeab1e44c5e03e
Luca Grigore <lucagrigore28@gmail.com>
2026-01-08 12:36:46 +0000

    nu tema culorilor este problema , ci problema este ca eu bifez toate mat

---
+++ src/firebase/auth/use-user.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { FirebaseContext, UserHookResult } from '@/firebase/provider';
+import { useContext } from 'react';
+
+/**
+ * Hook specifically for accessing the authenticated user's state.
+ * This provides the User object, loading status, and any auth errors.
+ * @returns {UserAuthHookResult} Object with user, isUserLoading, userError.
+ */
+export const useUser = (): UserHookResult => {
+  const context = useContext(FirebaseContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a FirebaseProvider.');
+  }
+
+  const { user, isUserLoading, userError } = context;
+  return { user, isUserLoading, userError };
+};
