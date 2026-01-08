445303fd6de4a34fe0fef5886b2e1c7d13530707
Luca Grigore <lucagrigore28@gmail.com>
2026-01-08 17:19:37 +0000

    mi-a preluat tema dar nu a trecut mai departe la ecranul cu "Vezi temele

--- src/components/setup/StepNotifications.tsx
+++ src/components/setup/StepNotifications.tsx
@@ -10,6 +10,7 @@ import { BellRing, BellOff } from 'lucide-react';
 import { Switch } from '@/components/ui/switch';
 import { Input } from '../ui/input';
 import { Separator } from '../ui/separator';
+import { useRouter } from 'next/navigation';
 
 type StepProps = {
   onNext?: () => void;
@@ -18,6 +19,7 @@ type StepProps = {
 
 export default function StepNotifications({ onNext, onBack }: StepProps) {
   const context = useContext(AppContext);
+  const router = useRouter();
   const [permission, setPermission] = useState<'default' | 'granted' | 'denied'>('default');
   
   const notifications = context?.userData?.notifications;
@@ -105,6 +107,8 @@ export default function StepNotifications({ onNext, onBack }: StepProps) {
     context?.updateUser({ setupComplete: true });
     if(onNext) {
       onNext();
+    } else {
+        router.push('/');
     }
   };
 
