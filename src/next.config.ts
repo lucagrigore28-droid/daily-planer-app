92e19a1c283e08ee459846d74d6ed0546b5f301e
Luca Grigore <lucagrigore28@gmail.com>
2026-01-10 15:17:42 +0000

    17:06:03.528  ⚠ Invalid next.config.ts options detected: 17:06:03.529

---
+++ src/next.config.ts
@@ -0,0 +1,41 @@
+
+import type {NextConfig} from 'next';
+require('dotenv').config();
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  serverExternalPackages: ['sharp'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_VAPID_KEY: process.env.NEXT_PUBLIC_VAPID_KEY,
+  }
+};
+
+export default nextConfig;
