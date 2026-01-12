ab350b32c42de2471f7646e1b6aa06198e19f4dc
Luca Grigore <lucagrigore28@gmail.com>
2026-01-12 20:38:46 +0000

    Try fixing this error: `Runtime Error: useUser must be used within a Fir

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { AppProvider } from '@/contexts/AppContext';
 import { Toaster } from '@/components/ui/toaster';
 import ServiceWorkerRegistrar from '@/components/ServiceWorkerRegistrar';
 import { ThemeProvider } from '@/components/ThemeProvider';
-import { FirebaseClientProvider } from '@/components/FirebaseClientProvider';
+import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
