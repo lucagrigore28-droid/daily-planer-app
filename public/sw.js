d014380e6b089476fa533dcd175eadaeef44a6a6
Luca Grigore <lucagrigore28@gmail.com>
2026-01-16 14:24:06 +0000

    pot sa il modific pe acesta sw.js ca sa adaug ce am copiat din ala desca

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,1 @@
-// This is the service worker file for PushAlert.
-// It's recommended to download the file directly from your PushAlert dashboard
-// but this is the standard content.
-importScripts('https://cdn.pushalert.co/sw.js');
+importScripts('https://cdn.pushalert.co/sw-c0b003d5dfc2b57d96526e1bb26749a4.js');
